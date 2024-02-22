b9420a8f9fe8a21edf72fea9d15ed903f16a890a
Detective Pikachu <frenzyexists@gmail.com>
2024-02-22 08:57:20 -0400

    First Commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "math.h"
+#include "string.h"
+#include "stdbool.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "driver/gpio.h"
+#include <freertos/semphr.h>
+
+void app_main() {}
